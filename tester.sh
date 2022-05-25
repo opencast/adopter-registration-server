4e143ef0a337661ceff905edeebbe94671a0c3a8
Greg Logan <gregorydlogan@gmail.com>
2022-05-25 14:37:53 -0600

    Deleting the testing data with the tester script, clarifying that this is normal in the output

--- tester.sh
+++ tester.sh
@@ -16,5 +16,10 @@ do
     echo '{"statistic_key": "'$stat_uuid'", "job_count": 0, "event_count": 0, "series_count": 0, "user_count": 1, "ca_count": 47, "total_minutes": 1000000, "tenant_count": 4, "hosts": [{"cores": 4, "max_load": 4.0, "memory": 1073741824, "hostname": "asdf", "services": "service1,\nservice2,\nservice3,\nservice4"}, {"cores": 2, "maxLoad": 20.0, "memory": 4, "hostname": "qwerty", "disk_space": -1}], "version": "9.0.0.SNAPSHOT", "adopter_key": "'$adopter_uuid'"}' > $TMP_STAT
     curl --header "Content-Type: application/json; utf-8" --data "@$TMP_STAT" http://localhost:5000/api/1.0/statistic
     rm -f $TMP_STAT
+    curl --header "Content-Type: application/json; utf8" -X DELETE --data '{"statistic_key": "'$stat_uuid'"}' http://localhost:5000/api/1.0/statistic
   done
+
+  curl --header "Content-Type: application/json; utf8" -X DELETE --data '{"adopter_key": "'$adopter_uuid'"}' http://localhost:5000/api/1.0/adopter
 done
+
+echo "There should be no records in the database now.  Modify the code above (removing the DELETEs) to see example data."
